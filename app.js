199c478c4c9a9965b04b9e0a58ffd4d95821f045
Baptiste <Baptiste.grias@gmail.com>
2022-09-23 10:35:44 -0700

    add encrier + liquide effect + fill from bottom to top text

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const percentage = document.querySelector('.percentage');
 let count = 0;
 
 window.addEventListener('load', () => {
-  let loading = setInterval(animate, 220);
+  let loading = setInterval(animate, 355);
 
   function animate() {
-    if(count === 100) {
+    if (count === 100) {
       setInterval(loading);
     } else {
       count++;
@@ -15,9 +15,3 @@ window.addEventListener('load', () => {
   }
 });
 
-
-// const logo = document.querySelectorAll('#logo path'); 
-
-// for(let i = 0; i < logo.length; i++) {
-//   console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-// }
